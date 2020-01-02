c549c784c0ade970c67d550019d63ef44f23c0f6
GourdErwa <gourderwa@163.com>
2020-01-02 18:31:25 +0800

    update

--- java-core/src/main/java/io/gourd/java/core/map/HashMapConflictingHash.java
+++ java-core/src/main/java/io/gourd/java/core/map/HashMapConflictingHash.java
@@ -2,31 +2,32 @@ package io.gourd.java.core.map;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.extern.slf4j.Slf4j;
+import lombok.ToString;
+import org.openjdk.jol.info.GraphLayout;
 
 import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.Map;
 
 /**
- * 模拟 HashMap hash 冲突后 HashMap 内部 table 数据结构及其他变量实时变化情况 .
+ * 模拟 HashMap hash 冲突后 HashMap 内部 table 数据结构及其他变量实时变化情况
  * - table 的扩容情况
  * - table 中节点，由链表转换为红黑树的变换条件
  * - loadFactor、size、threshold 实时变化情况
  *
  * <p>
- * 具体请参考 main 函数，执行打印结果为主
+ * 具体请参考 main 函数逻辑，执行后以打印结果为主
  *
  * @author Li.Wei by 2020/1/1
  */
-@Slf4j
-public class HashMapExample {
+public class HashMapConflictingHash {
 
     /**
-     * The Map. 默认容量为 16 ，负载因子为 1 进行测试。
+     * The Map. 默认容量为 16 ，负载因子为 0.75 进行测试。
      * 我们可以根据实际情况修改初始化参数和 {@link ConflictingHash#hashCode()} 方法进行碰撞测试
      */
-    public final Map<ConflictingHash, Integer> map = new HashMap<>(16, 1f);
+    @Getter
+    public final Map<ConflictingHash, Integer> map = new HashMap<>(16, 0.75f);
 
     /* ---------------- 反射 HashMap 内部私有变量字段 -------------- */
     private final Class<? extends Map> mapClass = map.getClass();
@@ -42,9 +43,10 @@ public class HashMapExample {
         sizeField.setAccessible(true);
     }
 
-    private HashMapExample()
+
+    private HashMapConflictingHash()
         throws ReflectiveOperationException {
-        log.info("loadFactor = {}", loadFactorField.get(map));
+        System.out.println("loadFactor = " + loadFactorField.get(map));
     }
 
 
@@ -54,18 +56,19 @@ public class HashMapExample {
      * @return the hash map example
      * @throws IllegalAccessException the illegal access exception
      */
-    public HashMapExample logFieldValue() throws IllegalAccessException {
-        log.info("---------------------------------");
-        Object arg = tableField.get(map);
-        Object[] tableArray = arg == null ? new Object[]{} : ((Object[]) arg);
-        log.info("table      = {}", tableArrayToString(tableArray));
+    public HashMapConflictingHash logMsg() throws IllegalAccessException {
+        System.out.println("-----------------------------------------------");
+        final Object arg = tableField.get(map);
+        final Object[] tableArray = arg == null ? null : ((Object[]) arg);
+        System.out.println("table      = " + tableArrayToString(tableArray));
 
-        log.info("threshold  = {}", thresholdField.get(map));
-        log.info("size       = {}", sizeField.get(map));
+        System.out.println("threshold  = " + thresholdField.get(map));
+        System.out.println("size       = " + sizeField.get(map));
+        System.out.println("-----------------------------------------------");
         return this;
     }
 
-    private HashMapExample put(ConflictingHash k, Integer v) {
+    private HashMapConflictingHash put(ConflictingHash k, Integer v) {
         map.put(k, v);
         return this;
     }
@@ -86,6 +89,8 @@ public class HashMapExample {
         for (int i = 0; i < a.length; i++) {
             final Object o = a[i];
             if (o != null) {
+                System.out.println("logMsg graphLayout table [" + i + "]，GraphLayout = " + GraphLayout
+                 .parseInstance(o).toPrintable());
                 b.append("，[").append(i).append("]=")
                     .append(o.getClass().getSimpleName())
                     .append(" ");
@@ -103,6 +108,7 @@ public class HashMapExample {
      */
     @AllArgsConstructor
     @Getter
+    @ToString
     static class ConflictingHash {
 
         private final int i;
@@ -115,20 +121,27 @@ public class HashMapExample {
             return i == that.i;
         }
 
+        // 因为此处取模后，左移 4 为，相当于放大 16 倍。最大为 48
+        // 48 相对于 HashMap#hash(Object) 函数来说，(h = key.hashCode()) ^ (h >>> 16) 结果不变。
         @Override
         public int hashCode() {
-            return i % 3;
+            return i % 4 << 4; // 等价于 (i % 4) * 16
         }
     }
 
     public static void main(String[] args)
         throws ReflectiveOperationException {
 
-        final HashMapExample example = new HashMapExample()
-            .logFieldValue();
+        final HashMapConflictingHash example = new HashMapConflictingHash()
+            .logMsg();
 
-        for (int i = 0; i < 30; i++) {
-            example.put(new ConflictingHash(i), i).logFieldValue();
+        for (int i = 1; i < 40; i++) {// put 次数
+
+            example.put(new ConflictingHash(i), i);
+            //if (i == 9 || i == 17)
+            example.logMsg();
         }
+
     }
+
 }
